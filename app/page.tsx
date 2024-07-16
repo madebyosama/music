59ee5ac949ff9544c0a1b288e6394ed7397e5cec
Muhammad Osama <madebyosama@gmail.com>
2024-07-16 19:11:33 +0500

    Update music

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,10 @@ export default function Home() {
         controls
         src='https://firebasestorage.googleapis.com/v0/b/uploadedbyosama.appspot.com/o/Music%2FEminem%20%E2%80%A2%20Tobey.mp3?alt=media&token=b3a1cbb6-a4c8-4536-91d4-471bca1ffc02'
       ></audio>
+      <audio
+        controls
+        src='https://firebasestorage.googleapis.com/v0/b/uploadedbyosama.appspot.com/o/Music%2FAli%20Zafar%20%E2%80%A2%20Paharon%20Ki%20Qasam.mp3?alt=media&token=3456a5af-30c7-4626-a247-fa1a2109dffa'
+      ></audio>
     </div>
   );
 }
