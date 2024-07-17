d3e47fbe0acbae951aa22aa67126d0fedc4ee32e
Muhammad Osama <madebyosama@gmail.com>
2024-07-17 10:05:43 +0500

    Update to single player

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ interface Song {
 export default function Home() {
   const [songs, setSongs] = useState<Song[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [currentSong, setCurrentSong] = useState<Song | null>(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -26,7 +27,7 @@ export default function Home() {
         }
         const data: Song[] = await response.json();
         setSongs(data);
-        console.log(data);
+        console.log('Fetched data:', data);
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
@@ -43,19 +44,26 @@ export default function Home() {
         <p>Loading...</p> // Display a loading indicator while fetching data
       ) : (
         <>
+          {currentSong && (
+            <audio controls src={currentSong.link} autoPlay></audio>
+          )}
           <div>
             {songs.map((song, index) => (
-              <div key={index} className={styles.song}>
+              <div
+                key={index}
+                className={styles.song}
+                onClick={() => setCurrentSong(song)}
+                style={{ cursor: 'pointer' }}
+              >
                 <Image
                   src={song.thumbnail}
                   alt={`${song.title} thumbnail`}
-                  width={20}
-                  height={20}
+                  width={50}
+                  height={50}
                 />
                 <div>
                   <h3>{song.title}</h3>
                   <p>{song.singer}</p>
-                  <audio controls src={song.link}></audio>
                 </div>
               </div>
             ))}
