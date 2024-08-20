ea716385c6a04b393d1fd3cfb122664497783a4f
Muhammad Osama <madebyosama@gmail.com>
2024-08-20 13:13:11 +0500

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -123,6 +123,18 @@ export default function Home() {
                 loading='lazy'
               ></iframe>
             </div>
+            <div>
+              <iframe
+                style={{ borderRadius: '12px' }}
+                src='https://open.spotify.com/embed/playlist/2cyykv0yTKzdqmA89srfCm?utm_source=generator&theme=0'
+                width='100%'
+                height='352'
+                frameBorder='0'
+                allowFullScreen
+                allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+                loading='lazy'
+              ></iframe>
+            </div>
           </div>
         </>
       )}
